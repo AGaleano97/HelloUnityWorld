ff1958f9406ad3cb681a552c30f25d791ee0e38f
AGaleano97 <52254706+AGaleano97@users.noreply.github.com>
2019-07-01 18:05:08 -0400

    First commit

---
+++ Hello Unity World/Assets/ChangeName.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ChangeName : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        TMPro.TextMeshProUGUI myText = GetComponent<TextMeshProUGUI>();
+        if (Input.GetKeyDown("space"))
+        {
+        myText.SetText("Hello World! My name is Alejandro");
+        }
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            Application.Quit();
+        }
+
+    }
+}
